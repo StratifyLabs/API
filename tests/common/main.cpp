da3185aea66c8925730fc9aeb5c1b71e16848053
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-20 17:44:45 -0500

    Updates tests to use Test::Scope

--- tests/common/main.cpp
+++ tests/common/main.cpp
@@ -16,16 +16,14 @@ int main(int argc, char *argv[]) {
   printer::Printer printer;
 
   printer.set_verbose_level(cli.get_option("verbose"));
-
-  test::Test::initialize(test::Test::Initialize()
-                           .set_name(cli.get_name())
-                           .set_version(VERSION)
-                           .set_git_hash(CMSDK_GIT_HASH)
-                           .set_printer(&printer));
-
-  { UnitTest(cli.get_name()).execute(cli); }
-
-  test::Test::finalize();
+  {
+    auto scope
+      = test::Test::Scope<printer::Printer>(test::Test::Initialize()
+                                              .set_name(cli.get_name())
+                                              .set_version(VERSION)
+                                              .set_git_hash(CMSDK_GIT_HASH));
+    UnitTest(cli.get_name()).execute(cli);
+  }
 
   exit(test::Test::final_result() == false);
 
