c50facbf514769385bc3832808148d89ce86e7a9
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-11-24 15:13:09 -0600

    Various cleanup items. cherry-pick: main

--- libraries/SysAPI/src/TemporaryDirectory.cpp
+++ libraries/SysAPI/src/TemporaryDirectory.cpp
@@ -7,17 +7,17 @@
 using namespace sys;
 
 TemporaryDirectory::TemporaryDirectory(const var::StringView parent)
-  : m_path(
+  : path_resource{
     (parent.is_empty() ? System::user_data_path() : parent)
-    / chrono::ClockTime::get_system_time().to_unique_string()) {
-  fs::FileSystem().create_directory(m_path);
+    / chrono::ClockTime::get_system_time().to_unique_string(), &deleter} {
+  fs::FileSystem().create_directory(path_resource.value());
   if (is_error()) {
-    m_path.clear();
+    path_resource.set_value({});
   }
 }
 
-TemporaryDirectory::~TemporaryDirectory() {
-  if (!m_path.is_empty()) {
-    fs::FileSystem().remove_directory(m_path, fs::FileSystem::IsRecursive::yes);
+void TemporaryDirectory::deleter(var::PathString *path) {
+  if (path->string_view()) {
+    fs::FileSystem().remove_directory(*path, fs::FileSystem::IsRecursive::yes);
   }
 }
--- libraries/ThreadAPI/include/thread/Thread.hpp
+++ libraries/ThreadAPI/include/thread/Thread.hpp
@@ -3,8 +3,8 @@
 #ifndef THREADAPI_THREAD_THREAD_HPP
 #define THREADAPI_THREAD_THREAD_HPP
 
-#include <pthread.h>
 #include <csignal>
+#include <pthread.h>
 
 #include "Sched.hpp"
 #include "chrono/ClockTime.hpp"
@@ -15,7 +15,7 @@ class Thread : public api::ExecutionContext {
 public:
   enum class DetachState {
     joinable = PTHREAD_CREATE_JOINABLE,
-    detached  = PTHREAD_CREATE_DETACHED
+    detached = PTHREAD_CREATE_DETACHED
   };
   using Policy = Sched::Policy;
 
@@ -39,13 +39,12 @@ public:
     Attributes &set_stack_size(size_t value);
     API_NO_DISCARD int get_stack_size() const;
 
-
     Attributes &set_detach_state(DetachState value);
-    Attributes& set_joinable(){
+    Attributes &set_joinable() {
       return set_detach_state(DetachState::joinable);
     }
 
-    Attributes& set_detached(){
+    Attributes &set_detached() {
       return set_detach_state(DetachState::detached);
     }
 
@@ -66,43 +65,37 @@ public:
     friend class Thread;
     pthread_attr_t m_pthread_attr{};
   };
-  using Function = api::Function<void*()>;
+  using Function = api::Function<void *()>;
 
   struct Construct {
-    API_PMAZ(argument, Construct, void*, nullptr);
+    API_PMAZ(argument, Construct, void *, nullptr);
     API_PMAZ(function, Construct, function_t, nullptr);
     API_PMAZ(thread_function, Construct, Function, {});
   };
 
   Thread() = default;
-
-  // don't allow making copies
-  Thread(const Thread &thread) = delete;
-  Thread &operator=(const Thread &thread) = delete;
-
-  // allow moving threads
-  Thread &operator=(Thread &&a)  noexcept {
-    swap(std::move(a));
-    return *this;
-  }
-  Thread(Thread &&a)  noexcept { swap(std::move(a)); }
   Thread &&move() { return std::move(*this); }
-
 
   explicit Thread(const Construct &options);
   Thread(const Attributes &attributes, const Construct &options);
-  Thread(const Attributes &attributes, void * argument, function_t thread_function){
-    construct(attributes, Construct().set_argument(argument).set_function(thread_function));
+  Thread(
+    const Attributes &attributes,
+    void *argument,
+    function_t thread_function) {
+    construct(
+      attributes,
+      Construct().set_argument(argument).set_function(thread_function));
   }
-  Thread(void * argument, function_t thread_function){
-    construct(Attributes(), Construct().set_argument(argument).set_function(thread_function));
+  Thread(void *argument, function_t thread_function) {
+    construct(
+      Attributes(),
+      Construct().set_argument(argument).set_function(thread_function));
   }
-  explicit Thread(Function && function);
-  Thread(const Attributes &attributes, Function && function);
-  ~Thread();
+  explicit Thread(Function &&function);
+  Thread(const Attributes &attributes, Function &&function);
 
   /*! \details Gets the ID of the thread. */
-  API_NO_DISCARD pthread_t id() const { return m_id; }
+  API_NO_DISCARD pthread_t id() const { return context->id; }
 
   /*! \details Returns true if the thread has a valid id.
    *
@@ -118,8 +111,7 @@ public:
     asynchronous = PTHREAD_CANCEL_ASYNCHRONOUS
   };
 
-  static const char * to_cstring(CancelType value);
-
+  static const char *to_cstring(CancelType value);
 
   static CancelType set_cancel_type(CancelType cancel_type);
   enum class CancelState {
@@ -127,7 +119,7 @@ public:
     disable = PTHREAD_CANCEL_DISABLE
   };
 
-  static const char * to_cstring(CancelState value);
+  static const char *to_cstring(CancelState value);
 
   Thread &set_sched_parameters(Sched::Policy policy, int priority);
   API_NO_DISCARD Sched::Policy get_sched_policy() const;
@@ -141,29 +133,28 @@ public:
   API_NO_DISCARD bool is_running() const;
   API_NO_DISCARD static pthread_t self() { return pthread_self(); }
 
-  const Thread &kill(int signal_number) const {
-    API_RETURN_VALUE_IF_ERROR(*this);
-    API_SYSTEM_CALL("", pthread_kill(id(), signal_number));
-    return *this;
+  const Thread &kill(int signal_number) const;
+
+  Thread &join(void **value = nullptr) &;
+  Thread &&join(void **value = nullptr) && { return std::move(join(value)); }
+
+  API_NO_DISCARD bool is_joinable() const {
+    return context->state == State::joinable;
   }
-
-  Thread &join(void **value = nullptr);
-
-  API_NO_DISCARD bool is_joinable() const { return m_state == State::joinable; }
 
 private:
+
   enum class State { null = 0, completed, error, joinable, detached };
+  struct Context {
+    pthread_t id = {};
+    State state = State::null;
+    void * signature = nullptr;
+  };
 
-  volatile State m_state = State::null;
+  static void deleter(Context *context);
+  api::SystemResource<Context, decltype(&deleter)> context;
 
-  pthread_t m_id = {};
-
-  void swap(Thread &&a) {
-    std::swap(m_id, a.m_id);
-    std::swap(m_state, a.m_state);
-  }
-
-  void construct(const Attributes & attributes, const Construct & options);
+  void construct(const Attributes &attributes, const Construct &options);
 
   int get_sched_parameters(int &policy, int &priority) const;
 };
